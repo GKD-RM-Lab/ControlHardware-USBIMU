315b901eeca94e6356233818b5a21f9b76c5e685
Fish-SWA <zhonghengyu2@gmail.com>
2024-10-08 19:47:03 +0800

    可以正常读取LSM6D的ID

--- Core/Src/app_freertos.c
+++ Core/Src/app_freertos.c
@@ -128,6 +128,17 @@ LSM6D读取相关
 */
 
 int cs_en = 1;
+/*默认传感器配置*/
+lsm6dso_fs_xl_t acc_scale = LSM6DSO_4g;
+lsm6dso_fs_g_t gyro_scale = LSM6DSO_500dps;
+lsm6dso_odr_xl_t acc_odr = LSM6DSO_XL_ODR_104Hz;
+lsm6dso_bdr_xl_t acc_batch_odr = LSM6DSO_XL_BATCHED_AT_104Hz;
+lsm6dso_odr_g_t gyro_odr = LSM6DSO_GY_ODR_104Hz;
+lsm6dso_bdr_gy_t gyro_batch_odr = LSM6DSO_GY_BATCHED_AT_104Hz;
+
+lsm6dso_shub_odr_t mag_sh_odr = LSM6DSO_SH_ODR_104Hz;
+
+int32_t fifo_size = 30;
 
 // SPI发送数据包装器函数
 int32_t SPI2_Send(void *handle, uint8_t reg, uint8_t *pData, uint16_t Length) {
@@ -162,6 +173,86 @@ int32_t SPI2_Recv(void *handle, uint8_t reg, uint8_t *pData, uint16_t Length) {
     return err;
 }
 
+/*LSM6D IO 结构体*/
+stmdev_ctx_t reg_ctx;
+
+
+/*用来存放读取到的数据*/
+float angular_rate_mdps[3];
+float acceleration_mg[3];
+float magnetic_mG[3];
+
+/*读取LSM6D数据*/
+void Lsm6dso_Sensor_update() {
+    uint16_t num = 114;
+    lsm6dso_fifo_tag_t reg_tag;
+    // timestamp_sample_t ts_tick;
+    uint32_t timestamp = 0.0f;
+    // axis3bit16_t dummy;
+
+    /* get sample sum */
+    lsm6dso_fifo_data_level_get(&reg_ctx, &num);
+    cprintf(&huart3, "%d\n", num);
+
+    // TODO: since f411 should be always catch up?
+    /* we should always try to catch up the latest sample */
+    // if (num > 0) {
+        
+    //     while (num--) {
+    //         /* Read FIFO tag. */
+    //         lsm6dso_fifo_sensor_tag_get(&reg_ctx, &reg_tag);
+
+    //         switch (reg_tag) {
+    //             case LSM6DSO_XL_NC_TAG:
+    //                 memset(data_raw_acceleration.u8bit, 0x00, 3 * sizeof(int16_t));
+    //                 lsm6dso_fifo_out_raw_get(&reg_ctx, data_raw_acceleration.u8bit);
+    //                 acceleration_mg[0] = lsm6dso_from_fs4_to_mg(data_raw_acceleration.i16bit[0]);
+    //                 acceleration_mg[1] = lsm6dso_from_fs4_to_mg(data_raw_acceleration.i16bit[1]);
+    //                 acceleration_mg[2] = lsm6dso_from_fs4_to_mg(data_raw_acceleration.i16bit[2]);
+    //                 break;
+
+    //             case LSM6DSO_GYRO_NC_TAG:
+    //                 memset(data_raw_angular_rate.u8bit, 0x00, 3 * sizeof(int16_t));
+    //                 lsm6dso_fifo_out_raw_get(&reg_ctx, data_raw_angular_rate.u8bit);
+    //                 angular_rate_mdps[0] = lsm6dso_from_fs500_to_mdps(data_raw_angular_rate.i16bit[0]);
+    //                 angular_rate_mdps[1] = lsm6dso_from_fs500_to_mdps(data_raw_angular_rate.i16bit[1]);
+    //                 angular_rate_mdps[2] = lsm6dso_from_fs500_to_mdps(data_raw_angular_rate.i16bit[2]);
+    //                 break;
+
+    //             case LSM6DSO_SENSORHUB_SLAVE0_TAG:
+    //                 memset(data_raw_magnetic.u8bit, 0x00, 3 * sizeof(int16_t));
+    //                 lsm6dso_fifo_out_raw_get(&reg_ctx, data_raw_magnetic.u8bit);
+	            
+    //                 // TODO: check & debug
+    //                 magnetic_mG[0] = normalizeRawMag(data_raw_magnetic.i16bit[0]);
+    //                 magnetic_mG[1] = normalizeRawMag(data_raw_magnetic.i16bit[1]);
+    //                 magnetic_mG[2] = normalizeRawMag(data_raw_magnetic.i16bit[2]);
+    //                 break;
+
+    //             // case LSM6DSO_TIMESTAMP_TAG:
+    //             //     lsm6dso_fifo_out_raw_get(&reg_ctx, ts_tick.byte);
+    //             //     timestamp = (unsigned int)lsm6dso_from_lsb_to_nsec(ts_tick.reg.tick);
+    //             //     break;
+
+    //             default:
+    //                 /* Flush unused samples. */
+    //                 memset(dummy.u8bit, 0x00, 3 * sizeof(int16_t));
+    //                 lsm6dso_fifo_out_raw_get(&reg_ctx, dummy.u8bit);
+    //                 break;
+    //         }
+
+    //         // Got sample
+    //         // if (has_new_acc && has_new_gyro && has_new_mag) {
+    //         if (has_new_acc && has_new_gyro) {
+    //             break;
+    //         }
+
+    //     }
+
+    // }
+
+}
+
 
 void StartDefaultTask(void *argument)
 {
@@ -172,7 +263,6 @@ void StartDefaultTask(void *argument)
       cprintf(&huart3, "SPI2 INIT ERROR");
   }
   // LSM6D 驱动结构体配置
-  stmdev_ctx_t reg_ctx;
   reg_ctx.write_reg = SPI2_Send;
   reg_ctx.read_reg = SPI2_Recv;
   reg_ctx.handle = &hspi2;
@@ -183,47 +273,39 @@ void StartDefaultTask(void *argument)
   lsm6dso_obj.Ctx = reg_ctx;
 
   // if (LSM6DSO_Init(&lsm6dso_obj) != LSM6DSO_OK) {
-  //     // Error_Handler();
   //     cprintf(&huart3, "LSM6D INIT ER\n");
   // }else{
   //     cprintf(&huart3, "LSM6D INIT OK\n");
   // }
+
+  //尝试读取ID
+  uint8_t id = 0x00;
+  if (LSM6DSO_ReadID(&lsm6dso_obj, &id) == 0) {
+    cprintf(&huart3, "id read ok, id = %x\n", id);
+    if(id == 0x6c) cprintf(&huart3, "ID OK\n");
+    else cprintf(&huart3, "ID NOT OK\n");
+  }else{
+    cprintf(&huart3, "id read fail\n");
+  }
+  
+  /*配置LSM6D*/
+  lsm6dso_i3c_disable_set(&reg_ctx, LSM6DSO_I3C_DISABLE);
+
+  int err = 0;
+  err = LSM6DSO_GYRO_Enable(&lsm6dso_obj);
+  cprintf(&huart3, "gyro init err = %d\n", err);
+  
+  LSM6DSO_GYRO_SetOutputDataRate(&lsm6dso_obj, LSM6DSO_XL_ODR_833Hz);  // 设置ODR
+  LSM6DSO_GYRO_Set_Power_Mode(&lsm6dso_obj, LSM6DSO_GY_HIGH_PERFORMANCE);  // 设置高性能模式
+  LSM6DSO_GYRO_SetFullScale(&lsm6dso_obj, LSM6DSO_250dps);  // 设置陀螺仪的量程
   
 
   for(;;)
   {
-    //尝试读取ID
-    uint8_t id = 114;
-    uint8_t reg_addr = 0x8F;
-    if (LSM6DSO_ReadID(&lsm6dso_obj, id) == 0) {
-      cprintf(&huart3, "id read ok, id = %d\n", id);
-    }else{
-      cprintf(&huart3, "id read fail\n");
-    }
-    // vTaskDelay(1);
-    // SPI2_Recv(&hspi2, 0x0F, &id, 1);
-    // HAL_GPIO_WritePin(LSM_CS_GPIO_Port, LSM_CS_Pin, GPIO_PIN_RESET);
-    // BSP_SPI2_Send(&reg_addr, 1);
-    // BSP_SPI2_Recv(id, 1);
-    // HAL_GPIO_WritePin(LSM_CS_GPIO_Port, LSM_CS_Pin, GPIO_PIN_SET);
-    //SPI TEST
-    // uint8_t data[5] = {0x01, 0x02, 0x03, 0x02, 0x03};
-    // volatile HAL_StatusTypeDef err_id =0;
-    // while(1){
-    //   // SPI2_Send(&hspi2, 0x5A, data, 3);
-    //   err_id = HAL_SPI_Transmit(&hspi2, data, 5, 0xFF);
-    //   vTaskDelay(1);
-    // }
-      //闪一次灯
-      HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-      vTaskDelay(5);
-      HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
-      vTaskDelay(5);
-    // cprintf(&huart3, "%d\n", status);
-    // cprintf(&huart3, "ok\n");
-
-    // HAL_UART_Transmit(&huart3, (uint8_t *)"ok\n", 4, 0xFFFF);
-
+    LSM6DSO_Axes_t gyro_data;
+    LSM6DSO_GYRO_GetAxes(&lsm6dso_obj, &gyro_data);
+    cprintf(&huart3, "Gyro X: %d, Y: %d, Z: %d\n", gyro_data.x, gyro_data.y, gyro_data.z);
+    vTaskDelay(100);
   }
   /* USER CODE END StartDefaultTask */
 }
