8e2ea6ea22bcf25bef8bc25dad639eef73ec1db0
Fish-SWA <zhonghengyu2@gmail.com>
2024-10-09 20:49:19 +0800

    引入CPP

---
+++ application/LSM6DSO_Task.cpp
@@ -0,0 +1,13 @@
+#include "LSM6DSO_Task.hpp"
+
+LSM6DSO_Handle IMU;
+
+int LSM6DSO_Task(void)
+{
+    while (1)
+    {
+        //do nothing
+    }
+    
+    return 0;
+}
---
+++ application/LSM6DSO_Task.hpp
@@ -0,0 +1,22 @@
+#ifndef LSM6DSO_Task_H
+#define LSM6DSO_Task_H
+
+class LSM6DSO_Handle
+{
+private:
+    /* data */
+public:
+    LSM6DSO_Handle(/* args */);
+    ~LSM6DSO_Handle();
+};
+
+LSM6DSO_Handle::LSM6DSO_Handle(/* args */)
+{
+}
+
+LSM6DSO_Handle::~LSM6DSO_Handle()
+{
+}
+
+
+#endif
