813c67f9d7196d52c74da74cb970640f01baaf94
Fish-SWA <zhonghengyu2@gmail.com>
2024-10-11 01:22:11 +0800

    可用的usb虚拟串口接收

--- application/USB_VCP_Task.cpp
+++ application/USB_VCP_Task.cpp
@@ -2,6 +2,7 @@
 #include "EKF_fusion.hpp"
 #include "LSM6DSO_Task.hpp"
 
+
 USB_VCPTask Usb;
 
 /*USB接收线程*/
@@ -9,7 +10,7 @@ void USB_VCP_RX_Task(void *argument)
 {
     while (1)
     {
-        cprintf(&huart3, "usb rx ok\n");
+        // cprintf(&huart3, "usb rx ok\n");
         vTaskDelay(1000);
     }
     
@@ -30,21 +31,82 @@ void USB_VCP_TX_Task(void *argument)
     
 }
 
-void USB_VCPTask::cdc_rx_handler(uint8_t *buf, uint8_t len)
+//USB接收回调函数
+void CDC_Receive_CallBack(uint8_t* pbuf, uint32_t *Len)
 {
-    // switch (buf[0])
-    // {
-    // case CMD_IMU_SET_FREQ:
-    //     break;
-    // case CMD_PWM_SET:
-    //     break;
-    // case CMD_LASER_SET:
-    //     break;
-    // case CMD_HARDWARE_INFO:
-    //     break;
-    // default:
-    //     break;
-    // }
+    uint8_t instruc_buf[32];
+    uint8_t instruc[32];        //提取到的指令
+    uint8_t len = *Len;
+    //帧头帧尾索引
+    uint8_t head_index = 0;
+    uint8_t tail_index = 0;
+    //解析到的命令ld
+
+
+    //把pbuf的前len个字节拷贝到instruc_buf
+    memcpy(instruc_buf, pbuf, len);
+    instruc_buf[len] = '\0';
+
+    // 寻找帧头尾
+    for(uint8_t i = 0; i < len; i++)
+    {
+        if(instruc_buf[i] == 0x5A) head_index = i;  //帧头
+        if(instruc_buf[i] == 0x9C) tail_index = i;  //帧尾
+    }
+    //无效帧返回
+    if(tail_index == 0) return;
+
+    //提取指令
+    memcpy(instruc, instruc_buf + head_index, tail_index - head_index + 1);
+
+    Usb.instruct_decode(instruc, tail_index - head_index + 1);
+}
+
+void USB_VCPTask::instruct_decode(uint8_t *buf, uint8_t len)
+{
+    Frame_freq_type imu_set_frame;
+    Frame_pwm_type pwm_set_frame;
+    Frame_laser_type laser_set_frame;
+    Frame_hardware_info_type hardware_info_frame;
+
+    //根据ID解析指令
+    switch (buf[1])
+    {
+    case 0x02: // CMD_IMU_SET_FREQ
+        memcpy(&imu_set_frame, buf, len);
+        //输出imu_set_frame全部信息
+        cprintf(&huart3, "head:%x, length:%d, cmd:%x, freq:%d, end:%x\n"
+                , imu_set_frame.head, imu_set_frame.length, imu_set_frame.cmd
+                , imu_set_frame.freq, imu_set_frame.end);
+
+        //todo:设置IMU发送频率
+        break;
+
+    case 0x03: // CMD_PWM_SET
+        memcpy(&pwm_set_frame, buf, len);
+        //输出pwm_set_frame全部信息
+        cprintf(&huart3, "head:%x, length:%d, cmd:%x, pwm:%d, %d, %d, %d, end:%x\n"
+                , pwm_set_frame.head, pwm_set_frame.length, pwm_set_frame.cmd
+                , pwm_set_frame.pwm[0], pwm_set_frame.pwm[1], pwm_set_frame.pwm[2], pwm_set_frame.pwm[3], pwm_set_frame.end);
+
+        break;
+
+    case 0x04: // CMD_LASER_SET
+        memcpy(&laser_set_frame, buf, len);
+        //输出laser_set_frame全部信息
+        cprintf(&huart3, "head:%x, length:%d, cmd:%x, laser:%d, end:%x\n"
+                , laser_set_frame.head, laser_set_frame.length, laser_set_frame.cmd
+                , laser_set_frame.laser, laser_set_frame.end);
+
+        
+        break;
+        
+    case 0x05: // CMD_HARDWARE_INFO
+        //todo:响应查询
+        break;
+    default:
+        break;
+    }
 }
 
 void USB_VCPTask::imu_angle_send(float *angle)
--- application/USB_VCP_Task.hpp
+++ application/USB_VCP_Task.hpp
@@ -40,6 +40,9 @@
 #include "usb_device.h"
 
 /*帧命令ID定义*/
+#define FRAME_HEAD 0x5A;            //帧头
+#define FRAME_TAIL 0x9C;            //帧尾
+
 #define CMD_IMU_TX 0x01;            //IMU数据发送帧
 #define CMD_IMU_SET_FREQ 0x02;      //IMU数据发送频率设置
 #define CMD_PWM_SET 0x03;           //PWM占空比设置
@@ -49,15 +52,56 @@
 /*IMU发送数据包结构体定义*/
 typedef struct
 {
-    uint8_t head = 0x5A;    //帧头
-    uint8_t length;         //数据长度, 之后初始化，根据数据长度用sizeof计算
-    uint8_t cmd = CMD_IMU_TX;            //命令ID
+    uint8_t head = FRAME_HEAD;              //帧头
+    uint8_t cmd = CMD_IMU_TX;               //命令ID
+    uint8_t length;                         //数据长度, 之后初始化，根据数据长度用sizeof计算
     float yaw;
     float pitch;
     float roll;
-    uint8_t end = 0x9C;     //帧尾
+    uint8_t end = FRAME_TAIL;               //帧尾
 }__attribute__((packed)) Frame_type;
 Frame_type frame;
+
+/*设定IMU回报频率数据包结构体*/
+typedef struct
+{
+    uint8_t head;        //帧头
+    uint8_t cmd;        //命令ID
+    uint8_t length;
+    uint16_t freq;       //频率(单位Hz)
+    uint8_t end;        //帧尾
+}__attribute__((packed)) Frame_freq_type;
+
+/*设定四个通道PWM占空比的数据包结构体*/
+typedef struct
+{
+    uint8_t head;        //帧头
+    uint8_t cmd;        //命令ID
+    uint8_t length;
+    uint16_t pwm[4];    //四个通道的PWM占空比
+    uint8_t end;        //帧尾
+}__attribute__((packed)) Frame_pwm_type;
+
+/*设定红点激光开关的数据包结构体*/
+typedef struct
+{
+    uint8_t head;        //帧头
+    uint8_t cmd;        //命令ID
+    uint8_t length;
+    uint8_t laser;      //激光开关
+    uint8_t end;        //帧尾
+}__attribute__((packed)) Frame_laser_type;
+
+/*硬件信息查询的数据包结构体*/
+typedef struct
+{
+    uint8_t head;        //帧头
+    uint8_t cmd;        //命令ID
+    uint8_t length;
+    uint8_t hardware_id;    //硬件ID
+    uint8_t firmware_version; //固件版本
+    uint8_t end;        //帧尾
+}__attribute__((packed)) Frame_hardware_info_type;
 
 
 class USB_VCPTask
@@ -65,7 +109,7 @@ class USB_VCPTask
 private:
     /* data */
 public:
-    void cdc_rx_handler(uint8_t *buf, uint8_t len);    //处理接收到的数据
+    void instruct_decode(uint8_t *buf, uint8_t len);    //处理接收到的数据
     void imu_angle_send(float *angle);
 };
 
@@ -80,6 +124,7 @@ extern "C" {
 
 void USB_VCP_RX_Task(void *argument);
 void USB_VCP_TX_Task(void *argument);
+void CDC_Receive_CallBack(uint8_t* pbuf, uint32_t *Len);
 
 #ifdef __cplusplus
 }
